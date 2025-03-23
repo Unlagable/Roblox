01294c6c8799f56128b3bc1761b919fc0c9db25d
Unlagable <84954390+Unlagable@users.noreply.github.com>
2025-03-24 01:16:18 +0700

    Update var.hpp

--- var.hpp
+++ var.hpp
@@ -1,5 +1,80 @@
-std::uint32_t ts = 0x0627D908;
-std::uint32_t lx = 0x127C9D8;
-std::uint32_t test1c4l = 0x0000112;
-std::uint32_t test1c1l = 0x133;
-helloworld
+constexpr std::uint32_t ts = 0x0627D908;
+constexpr std::uint32_t ts_st = 0x1d0;
+constexpr std::uint32_t ts_fin = 0x1d8;
+constexpr std::uint32_t js_N = 0x90;
+
+constexpr std::uint32_t rdv = 0x218;
+constexpr std::uint32_t rdve = 0x10;
+constexpr std::uint32_t dm0 = 0x120;
+constexpr std::uint32_t dmx = 0x1B8;
+
+constexpr std::uint32_t size = 0x8;
+constexpr std::uint32_t name = 0x70;
+constexpr std::uint32_t displayname = 0x110;
+constexpr std::uint32_t children = 0x78;
+constexpr std::uint32_t parent = 0x50;
+constexpr std::uint32_t local_player = 0x120;
+constexpr std::uint32_t model_instance = 0x2D0;
+constexpr std::uint32_t primitive = 0x168;
+constexpr std::uint32_t dimensions = 0x740;
+constexpr std::uint32_t viewmatrix = 0x4D0;
+
+constexpr std::uint32_t classname = 0x18;
+constexpr std::uint32_t health = 0x194;
+constexpr std::uint32_t max_health = 0x1B4;
+constexpr std::uint32_t walkspeed = 0x1d0;
+constexpr std::uint32_t walkspeed_2 = 0x3a8;
+constexpr std::uint32_t team = 0x200;
+constexpr std::uint32_t team_data = 0x118;
+constexpr std::uint32_t gameid = 0x168;
+constexpr std::uint32_t placeid = 0x178;
+constexpr std::uint32_t jobid = 0x118;
+
+constexpr std::uint32_t user_input_service = 0x398;
+constexpr std::uint32_t mouse_service = 0x278;
+constexpr std::uint32_t input_obj = 0x110;
+
+
+constexpr std::uint32_t camera_pos = 0x104;
+constexpr std::uint32_t camera = 0x3F0;
+constexpr std::uint32_t camera_rotation = 0xE0;
+constexpr std::uint32_t camera_cframe = 0xE0;
+constexpr std::uint32_t camera_fov = 0x148;
+
+
+constexpr std::uint32_t input_object_from_mouse = 0x170;
+
+constexpr std::uint32_t mouse_x = 0xEC;
+constexpr std::uint32_t mouse_y = mouse_x + 0x4;
+
+constexpr std::uint32_t anchored = 0x309;
+constexpr std::uint32_t can_collide = 0x310;
+constexpr std::uint32_t color = 0x190;
+constexpr std::uint32_t humanoid_jump = 0x1D1;
+constexpr std::uint32_t humanoid_sit = 0x1d4;
+constexpr std::uint32_t game_loaded = 0x3b1;
+constexpr std::uint32_t text_label_text = 0x698;
+constexpr std::uint32_t user_id = 0x210;
+
+
+constexpr std::uint32_t mouse = 0xAF8;
+constexpr std::uint32_t walkspeed_a = 0x1d0;
+constexpr std::uint32_t walkspeed_b = 0x3a8;
+constexpr std::uint32_t armor = 0xD0;
+constexpr std::uint32_t jumppower = 0x1B0;
+constexpr std::uint32_t jumpheight = 0x1B0 - 0x8;
+constexpr std::uint32_t hipheight = 0x1A0;
+constexpr std::uint32_t ping = 0xC0;
+constexpr std::uint32_t datamodel = 0x1B8;
+constexpr std::uint32_t humanoid_instance = 0x60;
+constexpr std::uint32_t brightness_2 = 0xad;
+constexpr std::uint32_t autojumpenable = 0x1cc;			//false = 01010100; t = 01010101
+
+constexpr std::uint32_t velocity = 0x14C;
+constexpr std::uint32_t position = 0x140;
+constexpr std::uint32_t move_direction = 0x158;
+constexpr std::uint32_t primsize = 0x2b4;
+constexpr std::uint32_t basepartsize = 0x2ac;
+constexpr std::uint32_t cframe = 0x11c;
+constexpr std::uint32_t rotation = 0x11c;
+constexpr std::uint32_t rotation_velocity = 0x158;
